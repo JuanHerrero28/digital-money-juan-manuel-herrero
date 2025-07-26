6fda78a0138a75f8f1ecf2e0879fbe26b947bc0d
JuanHerrero28 <juanmanuelherrero281991@gmail.com>
2025-07-26 17:38:48 -0300

    styled actualizado

---
+++ src/types/theme.ts
@@ -0,0 +1,12 @@
+export interface AppTheme {
+  colors: {
+    primary: string;
+    background: string;
+    text: string;
+  };
+  breakpoints: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+}
